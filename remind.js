c744e276168cba32836023d02ed63031885ffd51
danielrussellla <danielrussell16@gmail.com>
2017-09-06 20:04:39 -0700

    return out early and update errors

--- remind.js
+++ remind.js
@@ -21,13 +21,14 @@ if (!USER || !PR) {
     console.log('"user" or "pr" environment variables not defined.');
     console.log('Message not sent.');
     console.log('please run the command like: `user=bob pr=https://github.com/myorg/myorgrepo/pull/12345 node remind.js`')
+    return;
 }
 
 // gets all users and writes the json to a file - you can comment this block out once you run it once
 if (!fs.existsSync('./users.json')) {
     slack.api("users.list", (err, response) => {
         if (err) {
-            console.log('ERROR when fetching slack users', err);
+            console.log('ERROR fetching slack users', err);
             return;
         }
         fs.writeFile('users.json', JSON.stringify(response));
@@ -62,7 +63,7 @@ let remind = (userId) => {
 // read users.json and get the members array
 fs.readFile('./users.json', 'utf-8', (err, data) => {
     if (err) {
-        console.log('ERROR when reading user list', err);
+        console.log('ERROR reading user list', err);
         return;
     }
     let response = JSON.parse(data);
