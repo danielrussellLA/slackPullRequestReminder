c1494e4b84357d9d0c725eccfaf32538b60a5db4
danielrussellla <danielrussell16@gmail.com>
2017-09-06 20:20:38 -0700

    perfected app

--- remind.js
+++ remind.js
@@ -15,25 +15,12 @@ const slack = new Slack(apiToken); // init app
 let USER = process.env.user
 let PR = process.env.pr
 let FREQUENCY = 3600000 // 1 hour
-let FOUND_USER = true
 
 if (!USER || !PR) {
-    console.log('"user" or "pr" environment variables not defined.');
-    console.log('Message not sent.');
-    console.log('please run the command like: `user=bob pr=https://github.com/myorg/myorgrepo/pull/12345 node remind.js`')
+    console.log('ERROR: message not sent');
+    console.log('REASON: "user" or "pr" environment variables not defined.');
+    console.log('SOLUTION: Try running the command like: `user=bob pr=https://github.com/myorg/myorgrepo/pull/12345 node remind.js`')
     return;
-}
-
-// gets all users and writes the json to a file - you can comment this block out once you run it once
-if (!fs.existsSync('./users.json')) {
-    slack.api("users.list", (err, response) => {
-        if (err) {
-            console.log('ERROR fetching slack users', err);
-            return;
-        }
-        fs.writeFile('users.json', JSON.stringify(response));
-        console.log(response);
-    });
 }
 
 const messages = [
@@ -59,32 +46,24 @@ let remind = (userId) => {
     });
 }
 
-
-// read users.json and get the members array
-fs.readFile('./users.json', 'utf-8', (err, data) => {
+slack.api("users.list", (err, response) => {
     if (err) {
-        console.log('ERROR reading user list', err);
+        console.log('ERROR fetching slack users', err);
         return;
     }
-    let response = JSON.parse(data);
-    let members = response.members;
-    members.some( (member, i) => {
+    let users = response.members;
+    users.some( (member, i) => {
         if (USER.toLowerCase() === member.name.toLowerCase() ||
-            USER.toLowerCase() === member.profile.real_name.toLowerCase()) {
-                remind(member.id) // initial reminder
-                setInterval(() => {
-                    remind(member.id)
-                }, FREQUENCY) // repeat every hour
-                return true;
+        USER.toLowerCase() === member.profile.real_name.toLowerCase()) {
+            remind(member.id) // initial reminder
+            setInterval(() => {
+                remind(member.id)
+            }, FREQUENCY) // repeat reminder
+            return true;
         }
-        if (i === members.length -1) {
-            FOUND_USER = false;
+        if (i === users.length - 1) {
+            console.log('ERROR: could not find user:', USER);
+            console.log('- please enter a valid slack user');
         }
     });
-})
-
-if (!FOUND_USER) {
-    console.log('could not find user:', USER);
-    console.log('please enter a valid slack user');
-    return;
-}
+});
